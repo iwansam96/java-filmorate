48e1408ed5e415f53a83ae2d8a865cc10df63a21
samoylenko <samoylenko@lanit.ru>
2022-08-16 23:55:29 +0300

    fix: remove validate method

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,11 +2,9 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,44 +21,44 @@ public class FilmController {
     @PostMapping("/films")
     public Film create(@Valid @RequestBody Film film) {
         log.info("Получен запрос к эндпоинту: POST /films");
-        try {
-            validate(film);
+//        try {
+//            validate(film);
             films.put(film.getId(), film);
-        }
-        catch (ValidationException e) {
-            log.warn(e.getMessage());
-        }
+//        }
+//        catch (ValidationException e) {
+//            log.warn(e.getMessage());
+//        }
         return film;
     }
 
     @PutMapping("/films")
     public Film update(@Valid @RequestBody Film film) {
         log.info("Получен запрос к эндпоинту: PUT /films");
-        try {
-            if (validate(film) != null && films.containsKey(film.getId()))
+//        try {
+//            if (validate(film) != null && films.containsKey(film.getId()))
                 films.put(film.getId(), film);
-            else {
-                log.warn("Id not found");
-            }
-        } catch (ValidationException e) {
-            log.warn(e.getMessage());
-        }
+//            else {
+//                log.warn("Id not found");
+//            }
+//        } catch (ValidationException e) {
+//            log.warn(e.getMessage());
+//        }
         return film;
     }
 
-    private Film validate(Film film) throws ValidationException {
-        if (film.getName().isBlank()) {
-            throw new ValidationException("Name is not valid: cannot be blank");
-        }
-        if (film.getDescription().length() > 200) {
-            throw new ValidationException("Description is not valid: max 200 symbols");
-        }
-        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            throw new ValidationException("Release date is not valid: cannot be before then 28.12.1895");
-        }
-        if (film.getDuration().isNegative()) {
-            throw new ValidationException("Duration is not valid: cannot be negative");
-        }
-        return film;
-    }
+//    private Film validate(Film film) throws ValidationException {
+//        if (film.getName().isBlank()) {
+//            throw new ValidationException("Name is not valid: cannot be blank");
+//        }
+//        if (film.getDescription().length() > 200) {
+//            throw new ValidationException("Description is not valid: max 200 symbols");
+//        }
+//        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+//            throw new ValidationException("Release date is not valid: cannot be before then 28.12.1895");
+//        }
+//        if (film.getDuration().isNegative()) {
+//            throw new ValidationException("Duration is not valid: cannot be negative");
+//        }
+//        return film;
+//    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,11 +2,9 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,40 +21,40 @@ public class UserController {
     @PostMapping("/users")
     public User create(@Valid @RequestBody User user) {
         log.info("Получен запрос к эндпоинту: POST /users");
-        try {
-            if (validate(user) != null) {
+//        try {
+//            if (validate(user) != null) {
                 users.put(user.getId(), user);
-            }
-        } catch (ValidationException e) {
-            log.warn(e.getMessage());
-        }
+//            }
+//        } catch (ValidationException e) {
+//            log.warn(e.getMessage());
+//        }
         return user;
     }
 
     @PutMapping("/users")
     public User update(@Valid @RequestBody User user) {
         log.info("Получен запрос к эндпоинту: PUT /users");
-        try {
-            if (validate(user) != null & users.containsKey(user.getId()))
+//        try {
+//            if (validate(user) != null & users.containsKey(user.getId()))
                 users.put(user.getId(), user);
-            else {
-                log.warn("Id not found");
-            }
-        } catch (ValidationException e) {
-            log.warn(e.getMessage());
-        }
+//            else {
+//                log.warn("Id not found");
+//            }
+//        } catch (ValidationException e) {
+//            log.warn(e.getMessage());
+//        }
         return user;
     }
 
-    private User validate(User user) throws ValidationException {
-        if (user.getEmail().isBlank() || !user.getEmail().contains("@"))
-            throw new ValidationException("Email is not valid");
-        if (user.getLogin().isBlank() || user.getLogin().contains(" "))
-            throw new ValidationException("Name is not valid");
-        if (user.getName().isBlank())
-            user.setName(user.getLogin());
-        if (user.getBirthday().isAfter(LocalDate.now()))
-            throw new ValidationException("Birthday cannot be in future");
-        return user;
-    }
+//    private User validate(User user) throws ValidationException {
+//        if (user.getEmail().isBlank() || !user.getEmail().contains("@"))
+//            throw new ValidationException("Email is not valid");
+//        if (user.getLogin().isBlank() || user.getLogin().contains(" "))
+//            throw new ValidationException("Name is not valid");
+//        if (user.getName().isBlank())
+//            user.setName(user.getLogin());
+//        if (user.getBirthday().isAfter(LocalDate.now()))
+//            throw new ValidationException("Birthday cannot be in future");
+//        return user;
+//    }
 }
