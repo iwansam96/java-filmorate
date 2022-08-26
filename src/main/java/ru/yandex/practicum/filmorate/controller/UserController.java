2b645875e14804f27724b74ace94f228e190be4c
samoylenko <samoylenko@lanit.ru>
2022-08-27 00:37:26 +0300

    feat: sprint 10 first iteration

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,44 +1,112 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.user.UserService;
 
 import javax.validation.Valid;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @Slf4j
 @RestController
 public class UserController {
-    private Map<Integer, User> users = new HashMap<>();
+    private final UserService service;
+
+    @Autowired
+    public UserController(UserService service) {
+        this.service = service;
+    }
 
     @GetMapping("/users")
     public Collection<User> getAll() {
-        return users.values();
+        return service.getAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getById(@PathVariable int id) {
+        var result = service.getById(id);
+        if (result == null) {
+            log.warn("User id {} not found", id);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return result;
     }
 
     @PostMapping("/users")
     public User create(@Valid @RequestBody User user) {
         log.info("Получен запрос к эндпоинту: POST /users");
-        if (user.getName().isBlank())
-            user.setName(user.getLogin());
-        users.put(user.getId(), user);
+        service.create(user);
         return user;
     }
 
     @PutMapping("/users")
     public User update(@Valid @RequestBody User user) {
         log.info("Получен запрос к эндпоинту: PUT /users");
-            if (users.containsKey(user.getId()))
-                users.put(user.getId(), user);
-            else {
-                log.warn("Id not found");
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
+        var result = service.update(user);
+        if (result == null) {
+            log.warn("Id not found");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
         return user;
+    }
+
+    @DeleteMapping("/users")
+    public User delete(@Valid @RequestBody User user) {
+        log.info("Получен запрос к эндпоинту: DELETE /users");
+        var result = service.delete(user);
+        if (result == null) {
+            log.warn("User with id {} to REMOVE not found", user.getId());
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return user;
+    }
+
+    @PutMapping("/users/{id}/friends/{friendId}")
+    public User addFriend(@PathVariable int id, @PathVariable int friendId) {
+        log.info("Получен запрос к эндпоинту: PUT /users/{}/friends/{}", id, friendId);
+        var result = service.addFriend(id, friendId);
+        if (result == null) {
+            log.warn("User with id {} or {} to ADD FRIEND not found", id, friendId);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return result;
+    }
+
+    @DeleteMapping("/users/{id}/friends/{friendId}")
+    public User deleteFriend(@PathVariable int id, @PathVariable int friendId) {
+        log.info("Получен запрос к эндпоинту: DELETE /users/{}/friends/{}", id, friendId);
+        var result = service.deleteFriend(id, friendId);
+        if (result == null) {
+            log.warn("User with id {} or {} to REMOVE FRIEND not found", id, friendId);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return result;
+    }
+
+    @GetMapping("/users/{id}/friends")
+    public Collection<User> getAllFriends(@PathVariable int id) {
+        log.info("Получен запрос к эндпоинту: GET /users/{}/friends/", id);
+        var result = service.getALLFriends(id);
+        if (result == null) {
+            log.warn("User with id {} to GET ALL FRIENDS not found", id);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return result;
+    }
+
+    @GetMapping("/users/{id}/friends/common/{otherId}")
+    public List<User> getMutualFriends(@PathVariable int id, @PathVariable int otherId) {
+        log.info("Получен запрос к эндпоинту: GET /users/{}/friends/common/{}", id, otherId);
+        var result = service.getMutualFriends(id, otherId);
+        if (result == null) {
+            log.warn("User with id {} or {} to GET MUTUAL FRIENDS not found", id, otherId);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return result;
     }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
@@ -0,0 +1,75 @@
+package ru.yandex.practicum.filmorate.service.film;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class FilmService {
+
+    private final FilmStorage storage;
+
+    @Autowired
+    public FilmService(FilmStorage storage) {
+        this.storage = storage;
+    }
+
+
+    //    Basic functions
+    public Collection<Film> getAll() {
+        return storage.getAll();
+    }
+
+    public Film getById(int id) {
+        var film = storage.getAll().stream().filter(f -> f.getId() == id).findFirst();
+        return film.orElse(null);
+    }
+
+    public Film create(Film film) {
+        return storage.create(film);
+    }
+
+    public Film update(Film film) {
+        return storage.update(film);
+    }
+
+    public Film delete(Film film) {
+        return storage.delete(film);
+    }
+
+    //    Business functions
+
+    public Film addLike(int filmId, int userId) {
+        Film film = this.getById(filmId);
+        var likes = film.getLikes();
+        var result = likes.add(userId);
+        if (!result)
+            return null;
+        else
+            film.setLikes(likes);
+        return film;
+    }
+
+    public Film deleteLike(int filmId, int userId) {
+        Film film = this.getById(filmId);
+        var likes = film.getLikes();
+        if (likes.contains(userId)) {
+            likes.remove(userId);
+        }
+        else
+            return null;
+        return film;
+    }
+
+    public List<Film> getTopLiked(int count) {
+        return storage.getAll().stream()
+                .sorted((f1, f2) -> f2.getLikes().size()-f1.getLikes().size())
+                .limit(count)
+                .collect(Collectors.toList());
+    }
+}
